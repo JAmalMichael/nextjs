f5f6cd14576e63670624598c51ddee567279c284
JAmalMichael <eshimi090@gmail.com>
2024-10-06 23:57:24 +0100

    catch all segment

---
+++ src/app/docs/[[...slugs]]/page.js
@@ -0,0 +1,12 @@
+export default function Docs({ params }) {
+  if (params.slugs?.length === 2) {
+    return (
+      <h1>
+        Viewing Docs for feature {params.slugs[0]} and concept {params.slugs[1]}
+      </h1>
+    );
+  } else if (params.slugs?.length === 1) {
+    return <h1>Viewing Docs for feature {params.slugs[1]}</h1>;
+  }
+  return <h1>Docs Home Page</h1>;
+}
